cb5dde92771a9d3dcd21eab9ea6c4278302f35e1
Pragma <raph17.poth@gmail.com>
2024-06-20 19:21:14 -0400

    [AJOUT] - Fonction pour dessiner la grille | [MODIF] - decallage -> allerA

--- jeuDeLaVie.py
+++ jeuDeLaVie.py
@@ -11,29 +11,69 @@ def verifierTailleGrille(tailleGrille):
         
     return True
 
-def decallage(x, taille):
+def allerA(cible, position):
     pu()
-    bk(x)
+
+    fd(cible[0] - position[0])
     lt(90)
-    fd(taille)
+    fd(cible[1] - position[1])
     rt(90)
+
     pd()
 
 def creerGrille(tailleGrille):
-    clear()
-
     if verifierTailleGrille(tailleGrille):
-        for posX in range(tailleGrille[0] + 1):
-            fd(tailleGrille[0] * tailleGrille[2])
-            decallage(tailleGrille[0] * tailleGrille[2], tailleGrille[2])
+        for posY in range(tailleGrille[1] + 1):
+            fd(tailleGrille[1] * tailleGrille[2])
+            allerA([0, (posY + 1) * tailleGrille[2]], [tailleGrille[1] * tailleGrille[2], posY * tailleGrille[2]])
 
         rt(90)
         pu()
         fd(tailleGrille[2])
         pd()
     
-        for posY in range(tailleGrille[1] + 1):
-            fd(tailleGrille[1] * tailleGrille[2])
-            decallage(tailleGrille[0] * tailleGrille[2], tailleGrille[2]) 
+        for posX in range(tailleGrille[1] + 1):
+            fd(tailleGrille[0] * tailleGrille[2])
+            allerA([0, (posX + 1) * tailleGrille[2]], [tailleGrille[0] * tailleGrille[2], posX * tailleGrille[2]])
 
-creerGrille([10, 10, 10])
+        lt(90)
+
+
+def dessinerGrille(tailleGrille, grille):
+    position = [1, 5]
+
+    pensize(8)
+    pu()
+
+    for ligne in grille:
+        for elem in ligne:
+            if elem:
+                pencolor(1, 0, 0)
+                pd()
+                fd(tailleGrille[2] - 2)
+                pu()
+                fd(2)
+
+            else:
+                pencolor(1, 1, 1)
+                pd()
+                fd(tailleGrille[2] - 2)
+                pu()
+                fd(2)
+
+            position[0] += tailleGrille[2]            
+
+        allerA([1, position[1] + tailleGrille[2]], position)
+        position[0] = 1
+        position[1] += tailleGrille[2]
+
+clear()
+
+tailleGrille = [20, 20, 10]
+
+# Change le centre de la grille
+allerA([-100, -100], [0, 0])
+
+creerGrille(tailleGrille)
+allerA([1, 5], [(tailleGrille[0] + 1) * tailleGrille[2], tailleGrille[1] * tailleGrille[2]])
+dessinerGrille(tailleGrille, [[True, False], [True, True]])
