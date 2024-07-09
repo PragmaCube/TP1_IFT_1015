7b78d1a7907f8cc2363ff7c472438236d1cffaaa
Pragma <raph17.poth@gmail.com>
2024-07-09 17:44:10 -0400

    [AJOUT] - Méthode jouer pour l'animation du jeu

--- jeuDeLaVie.py
+++ jeuDeLaVie.py
@@ -1,17 +1,25 @@
+###################################################################
+# IFT-1015 Programmation 1
+# Fichier : jeuDeLaVie.py
+# Auteurs : Ariyan Adabzadeh (20218141) et Raphaël Pothier (20244742)
+# Programme simulant le jeu de la vie de John Conway dans une grille
+# de taille finie.
+
 import random
-# import turtle
-
-def verifierTailleGrille(tailleGrille):
-    assert type(tailleGrille) == list
-    assert len(tailleGrille) == 3
-
-    for elem in tailleGrille:
-        assert type(elem) == int
-        assert elem > 0
-        
-    return True
 
 def allerA(cible, position):
+    """
+    Fonction permettant de déplacer le curseur vers une cible
+    depuis une position connue à l'avance. La direction de
+    départ a un impact sur le référentiel :
+    - Vers la droite : aucun impact sur le référentiel
+    - Vers le haut   : le référentiel subit une rotation de pi / 2
+    - Vers la gauche : le référentiel subit une rotation de pi
+    - Vers le bas    : le référentiel subit une rotation de - pi / 2
+
+    Avant chaque utilisation, il est conseillé de fixer la droite 
+    comme direction du curseur.
+    """
     pu()
 
     fd(cible[0] - position[0])
@@ -22,24 +30,79 @@ def allerA(cible, position):
     pd()
 
 def creerGrille(tailleGrille):
-    if verifierTailleGrille(tailleGrille):
-        for posY in range(tailleGrille[1] + 1):
-            fd(tailleGrille[1] * tailleGrille[2])
-            allerA([0, (posY + 1) * tailleGrille[2]], [tailleGrille[1] * tailleGrille[2], posY * tailleGrille[2]])
+    """
+    Fonction dessinant la grille en fonction des paramètres
+    initiaux.
+    """
 
-        rt(90)
-        pu()
-        fd(tailleGrille[2])
-        pd()
-    
-        for posX in range(tailleGrille[1] + 1):
-            fd(tailleGrille[0] * tailleGrille[2])
-            allerA([0, (posX + 1) * tailleGrille[2]], [tailleGrille[0] * tailleGrille[2], posX * tailleGrille[2]])
+    for posY in range(tailleGrille.ny + 1):
+        fd(tailleGrille.nx * tailleGrille.largeur)
+        allerA([0, (posY + 1) * tailleGrille.largeur], [tailleGrille.nx * tailleGrille.largeur, posY * tailleGrille.largeur])
 
-        lt(90)
+    rt(90)
+    pu()
+    fd(tailleGrille.largeur)
+    pd()
 
+    for posX in range(tailleGrille.nx + 1):
+        fd(tailleGrille.ny * tailleGrille.largeur)
+        allerA([0, (posX + 1) * tailleGrille.largeur], [tailleGrille.ny * tailleGrille.largeur, posX * tailleGrille.largeur])
+
+    lt(90)
+
+def genererGrille(tailleGrille):
+    """
+    Fonction permettant de générer un tableau 2D initialisé
+    avec des False (cellule morte).
+    """
+    ligne = [False] * tailleGrille.nx
+
+    grille = []
+
+    for i in range(tailleGrille.ny):
+        # Il est nécessaire d'instancier à chaque itération
+        # une nouvelle liste pour éviter que chaque élément
+        # de grille ne pointe vers la même variable.
+        grille.append(list(ligne))
+
+    return grille
+
+def init(grille):
+    """
+    Fonction diposant de manière aléatoire les cellules
+    vivantes à l'état initial.
+    """
+
+    # La fonction f : [0, 1] -> [0, 0.5] définie par
+    # f(x) = 0.4x + 0.1 est une bijection, ce qui assure
+    # de générer des nombres entre 0 et 0.5 de manière
+    # aléatoire.
+    pourcentageCellulesVivantes = random.random() * 0.4 + 0.1
+
+    nbrCellulesVivantes = round(pourcentageCellulesVivantes * len(grille) * len(grille[0]))
+    compteur = 0
+
+    # Tant que les cellules vivantes ne sont pas complètement
+    # disposées, on parcours la grille et on détermine, au hasard,
+    # si chaque cellule est vivante.
+    while compteur != int(nbrCellulesVivantes):
+        for posY in range(len(grille)):
+            for posX in range(len(grille[0])):
+                if random.random() <= nbrCellulesVivantes / (len(grille) * len(grille[0])) and not grille[posY][posX]:
+                    grille[posY][posX] = True
+                    compteur += 1
+
+                if compteur == int(nbrCellulesVivantes):
+                    return grille
+
+    return grille
 
 def dessinerGrille(tailleGrille, grille):
+    """
+    Fonction coloriant la grille en fonction de l'état
+    actuel donné par grille.
+    """
+
     position = [1, 5]
 
     pensize(8)
@@ -50,83 +113,94 @@ def dessinerGrille(tailleGrille, grille):
             if elem:
                 pencolor(1, 0, 0)
                 pd()
-                fd(tailleGrille[2] - 2)
+                fd(tailleGrille.largeur - 2)
                 pu()
                 fd(2)
 
             else:
                 pencolor(1, 1, 1)
                 pd()
-                fd(tailleGrille[2] - 2)
+                fd(tailleGrille.largeur - 2)
                 pu()
                 fd(2)
 
-            position[0] += tailleGrille[2]            
+            position[0] += tailleGrille.largeur            
 
-        allerA([1, position[1] + tailleGrille[2]], position)
+        allerA([1, position[1] + tailleGrille.largeur], position)
         position[0] = 1
-        position[1] += tailleGrille[2]
+        position[1] += tailleGrille.largeur
 
 def detectionCellulesVivantes(grille, position):
+    """
+    Fonction permettant de détecter le nombre de cellules vivantes
+    autour d'une position connue en fonction d'une grille de booléens.
+    """
     cellulesVivantes = 0
 
-    if position[0] == 0:
-        if position[1] == 0:
-            cellulesVivantes += grille[0][1] + grille[1][0] + grille[1][1]
-
-        elif position[1] == len(grille) - 1:
-            cellulesVivantes += grille[len(grille) - 2][0] + grille[len(grille) - 1][1] + grille[len(grille) - 2][1]
-
-        else:
-            cellulesVivantes += grille[position[1] + 1][0] + grille[position[1] - 1][0]
-
-            for posY in range(-1, 2):
-                cellulesVivantes += grille[position[1] + posY][1]
-    
-    elif position[0] == len(grille[0]) - 1:
-        if position[1] == 0:
-            cellulesVivantes += grille[0][len(grille[0]) - 2] + grille[1][len(grille[0]) - 1] + grille[1][len(grille[0]) - 2]
-
-        elif position[1] == len(grille) - 1:
-            cellulesVivantes += grille[len(grille) - 2][len(grille[0]) - 2] + grille[len(grille) - 1][len(grille[0]) - 2] + grille[len(grille) - 2][len(grille[0]) - 1]
-
-        else:
-            cellulesVivantes += grille[position[1] + 1][len(grille[0]) - 1] + grille[position[1] - 1][len(grille[0]) - 1]
-
-            for posY in range(-1, 2):
-                cellulesVivantes += grille[position[1] + posY][len(grille[0]) - 2]
-
-    else:
-        if position[1] == 0:
-            cellulesVivantes += grille[0][position[0] + 1] + grille[0][position[0] - 1]
-
-            for posX in range(-1, 2):
-                cellulesVivantes += grille[1][position[0] + posX]
-            
-            return 
-
-        elif position[1] == len(grille) - 1:
-            cellulesVivantes += grille[len(grille) - 1][position[0] + 1] + grille[len(grille) - 1][position[0] - 1]
-
-            for posX in range(-1, 2):
-                cellulesVivantes += grille[len(grille) - 2][position[0] + posX]
-
-        else:
-            cellulesVivantes += grille[position[1]][position[0] + 1] + grille[position[1]][position[0] - 1]
-
-            for posX in range(-1, 2):
-                cellulesVivantes += grille[position[1] + 1][position[0] + posX] + grille[position[1] - 1][position[0] + posX]
+    for posX in range(max(0, position[0] - 1), min(len(grille[0]) - 1, position[0] + 1) + 1):
+        for posY in range(max(0, position[1] - 1), min(len(grille) - 1, position[1] + 1) + 1):
+            if position != [posX, posY]:
+                cellulesVivantes += grille[posY][posX]
 
     return cellulesVivantes
 
+def tests():
+    """
+    Fonction de tests unitaires pour les fonctions supplémentaires.
+    """
 
-clear()
+    # Tests vérifiant si les détections de cellules vivantes fonctionnent
+    # peu importe la position choisie (coin, bord, autre).
+    assert detectionCellulesVivantes([[True, False, True], [False, True, False], [True, False, True]], [1, 1]) == 4
+    assert detectionCellulesVivantes([[True, False, True], [False, True, False], [True, False, True]], [1, 0]) == 3
+    assert detectionCellulesVivantes([[True, False, True], [False, True, False], [True, False, True]], [0, 0]) == 1
 
-tailleGrille = [20, 20, 10]
+    # Test vérifiant le bon déplacement du curseur selon la fonction allerA
+    # Ce test est uniquement visuel.
+    allerA([-100, -100], [0, 0])
 
-# Change le centre de la grille
-allerA([-100, -100], [0, 0])
 
-creerGrille(tailleGrille)
-allerA([1, 5], [(tailleGrille[0] + 1) * tailleGrille[2], tailleGrille[1] * tailleGrille[2]])
-dessinerGrille(tailleGrille, [[True, False], [True, True]])
+def jouer(tailleGrille):
+    """
+    Fonction affichant indéfiniment la grille et la mettant à
+    jour selon les règles précisées. Il est possible de mettre
+    un délai d'affichage entre chaque itération.
+    """
+
+    grille = init(genererGrille(tailleGrille))
+    
+    ht()
+
+    # Change le centre de la grille
+    allerA([-100, -100], [0, 0])
+
+    creerGrille(tailleGrille)
+    allerA([1, 5], [(tailleGrille.nx + 1) * tailleGrille.largeur, tailleGrille.ny * tailleGrille.largeur])
+
+    while True:        
+        dessinerGrille(tailleGrille, grille)
+
+        for posY in range(len(grille)):
+            for posX in range(len(grille[0])):
+                if detectionCellulesVivantes(grille, [posX, posY]) < 2:
+                    grille[posY][posX] = False
+
+                elif detectionCellulesVivantes(grille, [posX, posY]) > 3:
+                    grille[posY][posX] = False
+
+                elif detectionCellulesVivantes(grille, [posX, posY]) == 3:
+                    grille[posY][posX] = True
+
+        allerA([1, 5], [1, tailleGrille.nx * tailleGrille.largeur + 5])
+        pd()
+
+        # Ajuster au besoin pour distinguer plus nettement chaque
+        # état.
+        sleep(0.1)
+
+
+
+tailleGrille = struct(nx = 20, ny = 20, largeur = 10)
+
+jouer(tailleGrille)
+###################################################################
